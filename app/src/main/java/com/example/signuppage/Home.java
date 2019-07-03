332bf0dcd94d485d1c600baa49ef27eac7bb5d6c
CoolForMe0812 <J17025677@student.newinti.edu.my>
2019-07-03 16:48:50 +0800

    layout changed: redesign in profile view

--- app/src/main/java/com/example/signuppage/Home.java
+++ app/src/main/java/com/example/signuppage/Home.java
@@ -1,13 +1,29 @@
 package com.example.signuppage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Home.this, Interest.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
