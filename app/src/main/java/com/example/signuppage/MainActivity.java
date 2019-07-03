f550876408d98ed4513739ec1a2298a6593ef4eb
CoolForMe0812 <J17025677@student.newinti.edu.my>
2019-07-03 11:31:58 +0800

    first initial

---
+++ app/src/main/java/com/example/signuppage/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.signuppage;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton floatingActionButton;
+    EditText Text1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Post.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
